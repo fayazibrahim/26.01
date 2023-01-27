a0b7ee6b3c65d849af5b1a376312b7f6448905b9
Ibrahimov Fayaz <FayazaI@code.local>
2023-01-27 14:00:26 +0400

    task2

---
+++ 2/2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            {
+
+                int[] numbers = { 77, 3, 2, 11, 18, 36 };
+
+                int Max = numbers[0];
+
+                for (var i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] > Max)
+                    {
+                        Max = numbers[i];
+                    }
+                }
+
+                Console.WriteLine(Max);
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
